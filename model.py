19a768cbaf126f2469d8862e2760d6c91fb45811
Tinko <51355285+TinkoCoder@users.noreply.github.com>
2021-04-17 15:25:56 +0200

    Update model.py

--- model.py
+++ model.py
@@ -1,74 +1,73 @@
 # Raziskovalna-naloga.
 # Program model.py s komentarji.
 
-    import torch
-    import torch.nn as nn
-    import torch.optim as optim
-    import torch.nn.functional as F
-    import os
-    # Knjižnice za strojno učenje.
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import os
+# Knjižnice za strojno učenje.
 
 
-    class Linear_QNet(nn.Module):
-        # Razred nevronske mreže.
-        def __init__(self, input_size, hidden_size, output_size):
-            super().__init__()
-            self.linear1 = nn.Linear(input_size, hidden_size)
-            self.linear2 = nn.Linear(hidden_size, output_size)
-        # Inicializacija modela/nevronske mreže.
-        
-        def forward(self, x):
-            x = F.relu(self.linear1(x))
-            x = self.linear2(x)
-            return x
-        # Aktivacijska funkcija (ReLu).
+class Linear_QNet(nn.Module):
+    # Razred nevronske mreže.
+    def __init__(self, input_size, hidden_size, output_size):
+        super().__init__()
+        self.linear1 = nn.Linear(input_size, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, output_size)
+    # Inicializacija modela/nevronske mreže.
 
-        def save(self, file_name='model.pth'):
-            model_folder_path = './model'
-            if not os.path.exists(model_folder_path):
-                os.makedirs(model_folder_path)
+    def forward(self, x):
+        x = F.relu(self.linear1(x))
+        x = self.linear2(x)
+        return x
+    # Aktivacijska funkcija (ReLu).
 
-            file_name = os.path.join(model_folder_path, file_name)
-            print(file_name)
-            torch.save(self.state_dict(), file_name)
-        # Funkcija za shranjevanje modela.
+    def save(self, file_name='model.pth'):
+        model_folder_path = './model'
+        if not os.path.exists(model_folder_path):
+            os.makedirs(model_folder_path)
 
-    class QTrainer:
-        # Razred trenerja nevronske mreže.
-        def __init__(self, model, lr, gamma):
-            self.lr = lr
-            self.gamma = gamma
-            self.model = model
-            self.optimizer = optim.Adam(model.parameters(), lr=self.lr)
-            self.criterion = nn.MSELoss()  # loss function (mean squared error)
-        # Inicializacija.
-        
-        def train_step(self, state, action, reward, next_state, done):
-            state = torch.tensor(state, dtype=torch.float)
-            next_state = torch.tensor(next_state, dtype=torch.float)
-            action = torch.tensor(action, dtype=torch.long)
-            reward = torch.tensor(reward, dtype=torch.float)
-        # Trening.
-            if len(state.shape) == 1:
-                state = torch.unsqueeze(state, 0)
-                next_state = torch.unsqueeze(next_state, 0)
-                action = torch.unsqueeze(action, 0)
-                reward = torch.unsqueeze(reward, 0)
-                done = (done,)
-            # Predvidevane vrednosti Q z tedanjim stanjem (state).
-            pred = self.model(state)
-            target = pred.clone()
-            for idx in range(len(done)):
-                Q_new = reward[idx]
-                if not done[idx]:
-                    Q_new = reward[idx] + self.gamma * torch.max(self.model(next_state[idx]))
-                    # Funkcija za Q vrednost.
+        file_name = os.path.join(model_folder_path, file_name)
+        torch.save(self.state_dict(), file_name)
+    # Funkcija za shranjevanje modela.
 
-                target[idx][torch.argmax(action).item()] = Q_new
+class QTrainer:
+    # Razred trenerja nevronske mreže.
+    def __init__(self, model, lr, gamma):
+        self.lr = lr
+        self.gamma = gamma
+        self.model = model
+        self.optimizer = optim.Adam(model.parameters(), lr=self.lr)
+        self.criterion = nn.MSELoss()  # loss function (mean squared error)
+    # Inicializacija.
 
-            self.optimizer.zero_grad()
-            loss = self.criterion(target, pred)
-            loss.backward()
-            # Funkcija izgube.
+    def train_step(self, state, action, reward, next_state, done):
+        state = torch.tensor(state, dtype=torch.float)
+        next_state = torch.tensor(next_state, dtype=torch.float)
+        action = torch.tensor(action, dtype=torch.long)
+        reward = torch.tensor(reward, dtype=torch.float)
+    # Trening.
+        if len(state.shape) == 1:
+            state = torch.unsqueeze(state, 0)
+            next_state = torch.unsqueeze(next_state, 0)
+            action = torch.unsqueeze(action, 0)
+            reward = torch.unsqueeze(reward, 0)
+            done = (done,)
+        # Predvidevane vrednosti Q z tedanjim stanjem (state).
+        pred = self.model(state)
+        target = pred.clone()
+        for idx in range(len(done)):
+            Q_new = reward[idx]
+            if not done[idx]:
+                Q_new = reward[idx] + self.gamma * torch.max(self.model(next_state[idx]))
+                # Funkcija za Q vrednost.
 
-            self.optimizer.step()
+            target[idx][torch.argmax(action).item()] = Q_new
+
+        self.optimizer.zero_grad()
+        loss = self.criterion(target, pred)
+        loss.backward()
+        # Funkcija izgube.
+
+        self.optimizer.step()
